8d85e809fb318c09372e019d7a656907092c487d
Yujie Jin <arceascompany@gmail.com>
2026-07-17 15:36:08 -0400

    Added p, i, and d control methods to pid_node.

--- src/ros2_auv_pid/ros2_auv_pid/pid_node.py
+++ src/ros2_auv_pid/ros2_auv_pid/pid_node.py
@@ -6,6 +6,36 @@ class PID(Node):
     def __init__(self):
         super().__init__("pid_node")
 
+        #Variables
+        self.previous_error = -1.0
+
+        #Publisher?
+
+
+    #PID methods
+    def p_control(self, desired_position, measured_position):
+        Kp = 1.0
+        error = desired_position - measured_position
+        proportional = Kp * error
+        return proportional
+    
+    def i_control(self, desired_position, measured_position, dt):
+        Ki = 1.0
+        error = desired_position - measured_position
+        error_accumulator += error * dt # dt is the time since the last update
+        integral = Ki * error_accumulator
+        integral = min(Ki * error_accumulator, 1.0)
+        return integral
+    
+    def d_control(self, desired_position, measured_position, dt):
+        Kd = 1.0
+        error = desired_position - measured_position
+        if self.previous_error == -1.0: self.previous_error = error
+        derivative = Kd * (error - self.previous_error) / dt # dt is the time since the last update
+        self.previous_error = error
+        return derivative
+
+
 
 def main(args=None):
     rclpy.init(args=args)
