85021a874bf504f4e4349e6f53da77ed4fb8d909
Yujie Jin <arceascompany@gmail.com>
2026-07-17 20:34:38 -0400

    Added new arming node to arm the AUV before use.

--- src/ros2_auv_pid/setup.py
+++ src/ros2_auv_pid/setup.py
@@ -25,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'depth_movement_node = ros2_auv_pid.depth_movement_node:main',
+            'arming_node = ros2_auv_pid.arming_node:main',
         ],
     },
 )
