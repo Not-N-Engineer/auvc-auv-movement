d6bf1142e4020dcd83a3b8bcd498e60e86a9caac
Yujie Jin <112829758+Not-N-Engineer@users.noreply.github.com>
2026-07-21 15:03:35 -0400

    Update pid.py

--- src/ros2_auv_pid/ros2_auv_pid/pid.py
+++ src/ros2_auv_pid/ros2_auv_pid/pid.py
@@ -1,10 +1,9 @@
 # from std_msgs.msg import Float64
 
-def pid(desired_position, measured_position, previous_error, error_acc, dt, Kp = 1.0, Ki = 1.0, Kd = 1.0):
-    error = desired_position - measured_position
+def pid(previous_error, error, error_acc, dt, Kp = 1.0, Ki = 1.0, Kd = 1.0):
     p = p_control(Kp, error)
     i = i_control(Ki, error, error_acc, dt)
-    d= d_control(Kd, error, previous_error, dt)
+    d = d_control(Kd, error, previous_error, dt)
 
     pid = p + i + d
     return (pid, error)
